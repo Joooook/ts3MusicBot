a9a97b5f5850f689f51ca4e23f9cdfd6e520a6c6
Joooook <zhou39512@gmail.com>
2024-11-23 15:43:48 +0800

    Netease Api.

---
+++ NeteaseApi.py
@@ -0,0 +1,32 @@
+import requests
+
+
+class NeteaseApi:
+    def __init__(self,host):
+        self.host=host
+    def get_list(self,id):
+        url=self.host+"/playlist/track/all"
+        params={"id":id}
+        try:
+            rep=requests.get(url,params=params)
+            if rep.json()['code'] == 200:
+                songs = rep.json()['songs']
+                id_list = [song['id'] for song in songs]
+                song_list=self.get_songs(id_list)
+                return song_list
+        except Exception:
+            return None
+        return None
+
+    def get_songs(self,ids:list):
+        url=self.host+"/song/url/v1"
+        params={"id":','.join(map(str,ids)),'level':'standard'}
+        print(ids)
+        try:
+            rep=requests.get(url,params=params)
+            if rep.json()['code'] == 200:
+                songs = [song['url'] for song in rep.json()['data']]
+                return songs
+        except Exception:
+            return None
+        return None
