4a55f193877fd0aaec5ad45d07c3442abf61675e
Joooook <zhou39512@gmail.com>
2024-11-26 14:02:23 +0800

    Api reformat.

--- apis/audioBotApi/AudioBotApi.py
+++ apis/audioBotApi/AudioBotApi.py
@@ -29,8 +29,11 @@ class AudioBotApi:
         self.bot_id = bot_id
 
     @api
-    def exec(self, *args):
-        urlencoded_args = map(lambda x: urllib.parse.quote(x, encoding='UTF-8', safe=''), args)
+    def exec(self, *args, encode_all = True):
+        if encode_all:
+            urlencoded_args = map(lambda x: urllib.parse.quote(x, encoding='UTF-8', safe=''), args)
+        else:
+            urlencoded_args =map(lambda x: urllib.parse.quote(x, encoding='UTF-8'), args)
         url = self.url + f"/api/bot/use/{self.bot_id}/(/" + '/'.join(urlencoded_args)
         rep = requests.get(url)
         return AudioBotApiResponse.success(rep)
@@ -42,7 +45,7 @@ class AudioBotApi:
         response = self.exec("add", link)
         rep: requests.models.Response = response.data
         if rep.status_code != 204:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
     @api
@@ -50,15 +53,15 @@ class AudioBotApi:
         response = self.exec("list", "add", list_id, link)
         rep: requests.models.Response = response.data
         if rep.status_code != 204:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
     @api
     def play(self, link: str=''):
         response = self.exec("play", link)
         rep: requests.models.Response = response.data
-        if rep.status_code != 204:
-            raise AudioBotApiException
+        if rep.status_code != 204 and rep.status_code != 200:
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
     @api
@@ -66,7 +69,7 @@ class AudioBotApi:
         response = self.exec("pause")
         rep: requests.models.Response = response.data
         if rep.status_code != 204:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
     @api
@@ -74,7 +77,7 @@ class AudioBotApi:
         response = self.exec("previous")
         rep: requests.models.Response = response.data
         if rep.status_code != 204:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
     @api
@@ -82,7 +85,7 @@ class AudioBotApi:
         response = self.exec("next")
         rep: requests.models.Response = response.data
         if rep.status_code != 204:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
     @api
@@ -90,7 +93,15 @@ class AudioBotApi:
         response = self.exec("jump",str(index))
         rep: requests.models.Response = response.data
         if rep.status_code != 204:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
+        return AudioBotApiResponse.success()
+
+    @api
+    def seek(self, position: float):
+        response = self.exec("seek", str(position))
+        rep: requests.models.Response = response.data
+        if rep.status_code != 204:
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
     @api
@@ -98,22 +109,45 @@ class AudioBotApi:
         response = self.exec("clear")
         rep: requests.models.Response = response.data
         if rep.status_code != 204:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
+    @api
+    def stop(self):
+        response = self.exec("stop")
+        rep: requests.models.Response = response.data
+        if rep.status_code != 204:
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
+        return AudioBotApiResponse.success()
+
+    @api
+    def is_playing(self):
+        response = self.exec("song")
+        rep: requests.models.Response = response.data
+        if rep.status_code == 422 and rep.json()['ErrorCode']==10:
+            return AudioBotApiResponse.success(False)
+        elif rep.status_code == 200 and rep.json()['Length']==0.0:
+            return AudioBotApiResponse.success(False)
+        elif rep.status_code == 200 :
+            return AudioBotApiResponse.success(True)
+        return AudioBotApiResponse.failure("未能获取状态。")
+
+
     # 设置属性用set开头
+    @api
     def set_bot_avatar(self,link: str):
         response = self.exec("bot","avatar","set",link)
         rep: requests.models.Response = response.data
         if rep.status_code != 204:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
+    @api
     def set_bot_description(self,text: str):
         response = self.exec("bot","description","set",text)
         rep: requests.models.Response = response.data
         if rep.status_code != 204:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success()
 
 
@@ -124,7 +158,7 @@ class AudioBotApi:
         response = self.exec("song")
         rep: requests.models.Response = response.data
         if rep.status_code != 200:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success(rep.json())
 
     @api
@@ -132,7 +166,7 @@ class AudioBotApi:
         response = self.exec("song")
         rep: requests.models.Response = response.data
         if rep.status_code != 200:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success(rep.json())
 
     @api
@@ -140,14 +174,30 @@ class AudioBotApi:
         response = self.exec("bot", "info", "client")
         rep: requests.models.Response = response.data
         if rep.status_code != 200:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         return AudioBotApiResponse.success(rep.json()['Uid'])
+
+    @api
+    def get_clid(self):
+        response = self.exec("bot", "info", "client")
+        rep: requests.models.Response = response.data
+        if rep.status_code != 200:
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
+        return AudioBotApiResponse.success(rep.json()['Id'])
+
+    @api
+    def get_cid(self):
+        response = self.exec("bot", "info", "client")
+        rep: requests.models.Response = response.data
+        if rep.status_code != 200:
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
+        return AudioBotApiResponse.success(rep.json()['Channel'])
 
     @api
     def get_list_ids(self):
         response = self.exec('list', 'list')
         rep: requests.models.Response = response.data
         if rep.status_code != 200:
-            raise AudioBotApiException
+            return AudioBotApiResponse.failure(f"状态码错误：{rep.status_code}")
         res = [i['Id'] for i in rep.json()]
         return AudioBotApiResponse.success(res)
